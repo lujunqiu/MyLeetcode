314b12e8db9c911f47c652a7c4eb94c4e8c641bb
卢军球 <lujunqiu@lujunqiudeMacBook-Air.local>
2017-03-10 23:15:31 +0800

    gitignore update

---
+++ src/Test3_Longest_Substring_Without_Repeating_Characters.java
@@ -0,0 +1,14 @@
+/**
+ * Created by lujunqiu on 17/3/10.
+ * Description:
+ * Given a string, find the length of the longest substring without repeating characters.
+ * Examples:
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
+public class Test3_Longest_Substring_Without_Repeating_Characters {
+    public int lengthOfLongestSubstring(String s) {
+
+    }
+}
