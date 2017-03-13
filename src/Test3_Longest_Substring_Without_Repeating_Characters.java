07ba4e1039846167c18253843df5ee5a29b2359b
lujunqiu <2672543426@qq.com>
2017-03-13 09:55:13 +0800

    Update Test3_Longest_Substring_Without_Repeating_Characters.java

--- src/Test3_Longest_Substring_Without_Repeating_Characters.java
+++ src/Test3_Longest_Substring_Without_Repeating_Characters.java
@@ -89,7 +89,29 @@ public class Test3_Longest_Substring_Without_Repeating_Characters {
         }
         return len;
     }
-
+    
+    /*
+    最佳参考：Leetcode Editorial Solution。
+    In fact, it could be optimized to require only n steps.
+    Instead of using a set to tell if a character exists or not, we could define a mapping of the characters to its index.
+    Then we can skip the characters immediately when we found a repeated character.
+    The reason is that if s[j]s[j] have a duplicate in the range [i, j)[i,j) with index j'j′ , we don't need to increase ii little by little.
+    We can skip all the elements in the range [i, j'][i,j′ ] and let ii to be j' + 1j′ +1 directly.
+    */
+     public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+    
     public static void main(String[] args) {
         System.out.println(SlidingWindow("abcabcbb"));
         System.out.println(SlidingWindow("bbbbbb"));
