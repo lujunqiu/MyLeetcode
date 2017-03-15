3c67d0a9ab7288088edffe6267bca55eb82fd527
卢军球 <lujunqiu@lujunqiudeMacBook-Air.local>
2017-03-16 00:22:41 +0800

    new test 14

---
+++ src/Test_14_Longest_Common_Prefix.java
@@ -0,0 +1,10 @@
+/**
+ * Created by lujunqiu on 17/3/16.
+ * Description:
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ */
+public class Test_14_Longest_Common_Prefix {
+    public String longestCommonPrefix(String[] strs) {
+
+    }
+}
