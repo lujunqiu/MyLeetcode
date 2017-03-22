ccd76dc1fd96d043d5737f6f6eeb1fb3fc2f9dbf
卢军球 <lujunqiu@lujunqiudeMacBook-Air.local>
2017-03-22 19:15:18 +0800

    test 26 solution

---
+++ src/Test19_Remove_Nth_Node_From_End_of_List.java
@@ -0,0 +1,28 @@
+/**
+ * Created by lujunqiu on 17/3/22.
+ * Description:
+ * Given a linked list, remove the nth node from the end of list and return its head.
+ * For example,
+ * Given linked list: 1->2->3->4->5, and n = 2.
+ * After removing the second node from the end, the linked list becomes 1->2->3->5.
+ * Note:
+ * Given n will always be valid.
+ * Try to do this in one pass.
+ */
+public class Test19_Remove_Nth_Node_From_End_of_List {
+    /*
+    Definition for singly-linked list.
+     */
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        
+    }
+
+}
--- src/Test26_Remove_Duplicates_from_Sorted_Array.java
+++ src/Test26_Remove_Duplicates_from_Sorted_Array.java
@@ -9,11 +9,33 @@
  * It doesn't matter what you leave beyond the new length.
  */
 public class Test26_Remove_Duplicates_from_Sorted_Array {
-    public int removeDuplicates(int[] nums) {
+    /*
+    这个问题属于很基础的数组处理问题,思路也很清晰.时间复杂度O(n)
+    由于是已经排好序的数组,我们使用2个指针,一个记录不同元素的个数,一个遍历数组遇到相同元素就跳过.(tricks:遍历的时候灵活的运用指针(s)来记录信息.
+    另外,如果是无序的数组,我们可以使用hashset来记录数组找到不同元素.如果有空间复杂度的限制,那么就先排序(推荐快排),然后做相同的处理即可.
+     */
+    static public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int result = 1;
 
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] == nums[i]) {
+                continue;
+            } else {
+                nums[result] = nums[i];
+                result++;
+            }
+
+        }
+        return result;
     }
 
     public static void main(String[] args) {
+        int[] test = {1, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 9, 9, 9, 9, 9, 10};
+        int[] test1 = {1, 1, 2};
 
+        System.out.println(removeDuplicates(test));
     }
 }
