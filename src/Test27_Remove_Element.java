3b2a2696a5d804e7be1eaf6016a8b4f7a3ce3355
卢军球 <lujunqiu@lujunqiudeMacBook-Air.local>
2017-03-30 23:44:14 +0800

    solutions

--- src/Test27_Remove_Element.java
+++ src/Test27_Remove_Element.java
@@ -9,7 +9,36 @@
  * Your function should return length = 2, with the first two elements of nums being 2.
  */
 public class Test27_Remove_Element {
+    /*
+    我们要实现的是数组元素就地删除操作.使用2个指针,一个快指针一个慢指针,快指针遍历数组,慢指针在遇到要删除的元素会停顿下来后,然后把要删除元素的后序元素逐一前移覆盖.
+    我们在删除数组元素的过程中,仍然保持了数组元素的相对顺序.题目中说到"The order of elements can be changed.",说明还是可以优化的.接下来介绍一下Leetcode的Editorial Solution.
+     */
     public int removeElement(int[] nums, int val) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
 
+    /*
+    这里利用了题目中说到"The order of elements can be changed." 我们在遇到需要删除的元素的时候将其与当前数组最后一个元素替换,然后减少数组长度.
+    这样的话,删除的元素都会移动到数组的最后,但是其他元素的相对位置会有所改变.
+     */
+    public int EditorialSolution(int[] nums, int val) {
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                n--;
+            } else {
+                i++;
+            }
+        }
+        return n;
     }
 }
---
+++ src/Test70_Climbing_Stairs.java
@@ -0,0 +1,27 @@
+/**
+ * Created by lujunqiu on 17/3/30.
+ * Description:
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ * Note: Given n will be a positive integer.
+ */
+public class Test70_Climbing_Stairs {
+    /*
+    很简单,求解对应数值n的斐波那契数就好了.高中数学题...
+     */
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int[] result = new int[n + 1];
+        result[1] = 1;
+        result[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            result[i] = result[i - 1] + result[i - 2];
+        }
+        return result[n];
+    }
+}
