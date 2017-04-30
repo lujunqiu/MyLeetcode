ea96d8f218d522d594fbe7e9af78c2d3000af907
卢军球 <lujunqiu@lujunqiudeMacBook-Air.local>
2017-04-30 22:14:22 +0800

    sol 101

---
+++ src/Test101_Symmetric_Tree.java
@@ -0,0 +1,135 @@
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Queue;
+
+/**
+ * Created by lujunqiu on 17/4/25.
+ * Description:
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ * But the following [1,2,2,null,3,null,3] is not:
+ */
+public class Test101_Symmetric_Tree {
+    /*
+    我们给定了一个树的根节点,判断是否以根节点为中心是镜像对称的.
+
+    递归的做法:
+    首先判断根节点的左右子节点的值是否相等,再判断左右子树是否是镜像对称的即可.
+    我们设计的递归函数的输入值为2个"根节点",来判断2个"树"是否镜像对称.
+     */
+    static public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else {
+            return isSymmetric(root.left, root.right);
+        }
+    }
+
+    static private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null && right != null) {
+            return false;
+        }
+        if (left != null && right == null) {
+            return false;
+        }
+        if (left.val == right.val) {//递归地判断,左子树的右子树与右子树的左子树,以及左子树的左子树与右子树的右子树是否镜像对称
+            return isSymmetric(left.right, right.left) && isSymmetric(left.left, right.right);
+        } else {
+            return false;
+        }
+    }
+
+    /*
+    将上述递归的方法写成非递归的形式;
+    思路是一样的,使用2个栈来再现上述递归的过程.但是我第一次实现的非递归的代码虽然AC了,里面有太多的if else,看起来很别扭.....后面我会把Editorial Solution贴出来反思学习.
+     */
+    public boolean nonrecursive(TreeNode root) {
+        Stack<TreeNode> leftstack = new Stack<>();
+        Stack<TreeNode> rightstack = new Stack<>();
+        if (root == null) {
+            return true;
+        } else if (root.left != null && root.right != null) {
+            leftstack.push(root.left);
+            rightstack.push(root.right);
+        } else if (root.left == null && root.right == null) {
+            return true;
+        } else {
+            return false;
+        }
+        while (true) {
+            if (leftstack.empty() && rightstack.empty()) {
+                return true;
+            }
+            if (leftstack.empty() && !rightstack.empty()) {
+                return false;
+            }
+            if (!leftstack.empty() && rightstack.empty()) {
+                return false;
+            }
+            if (leftstack.peek().val == rightstack.peek().val) {
+                TreeNode a = leftstack.pop();
+                TreeNode b = rightstack.pop();
+                if (a.left != null) {
+                    leftstack.push(a.left);
+                } else {
+                    if (b.right != null) {
+                        return false;
+                    }
+                }
+                if (a.right != null) {
+                    leftstack.push(a.right);
+                } else {
+                    if (b.left != null) {
+                        return false;
+                    }
+                }
+                if (b.right != null) {
+                    rightstack.push(b.right);
+                }
+                if (b.left != null) {
+                    rightstack.push(b.left);
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+    /*
+    Leetcode Editorial Solution给出的非递归算法,比我实现的好看一万倍....
+    只用到了一个队列来实现,思路清晰!反观我的代码,考虑太多关于null的问题,导致代码if else很多.bad solution.
+    这里将所有return false的情况都列出来,剩下的统一return true,比上面我实现的代码科学很多,至少思路清晰.
+     */
+    public boolean Editorial(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            if (t1 == null && t2 == null) continue;
+            if (t1 == null || t2 == null) return false;
+            if (t1.val != t2.val) return false;
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t1.right);
+            q.add(t2.left);
+        }
+        return true;
+    }
+}
+
+/*
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
--- src/Test84_Largest_Rectangle_in_Histogram.java
+++ src/Test84_Largest_Rectangle_in_Histogram.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /**
  * Created by lujunqiu on 17/4/17.
  * Description:
@@ -8,7 +10,75 @@
  * return 10.
  */
 public class Test84_Largest_Rectangle_in_Histogram {
-    public int largestRectangleArea(int[] heights) {
-        return 0;
+    /*
+    状态定义:以height[i]的高度为最低高度的矩阵的面积记录为dp[i]
+    时间复杂度:O(n^2)
+    Leetcode提交后通过 95/96 case,最后一个case超时.
+     */
+    static public int largestRectangleArea(int[] heights) {
+        int[] dp = new int[heights.length];
+        int area = 0;
+
+        for (int i = 0; i < heights.length; i++) {
+            int left = i;
+            int right = i;
+            for (int j = i - 1; j >= 0 && heights[j] >= heights[i]; j--) {
+                left = j;
+            }
+            for (int j = i + 1; j <= heights.length - 1 && heights[j] >= heights[i]; j++) {
+                right = j;
+            }
+            dp[i] = (right - left + 1) * heights[i];
+            area = area > dp[i] ? area : dp[i];
+        }
+        // System.out.println(Arrays.toString(dp));
+        return area;
+    }
+
+    /*
+    似乎Leetcode要求的是O(n)的算法才能AC,我们需要进一步改进最初的O(n^2)的算法,增加我们的搜索解的效率.
+    我从google找到了一个该题的O(n)的算法,用到了栈.
+
+    思路是:
+    在遍历数组的时候维护一个栈,这个栈中的元素从底向上按照高度值递增.如果遇到当前bar高度比栈顶元素低，那么就出栈直到栈顶元素低于当前bar.
+
+    每次元素出栈都要做如下运算:
+    如果栈已经为空，矩阵面积就是高度乘以当前下标i。
+    如果栈不为空，矩阵面积就是高度乘以当前下标i减栈顶元素再减1。
+
+    在遍历完数组后,仍然要继续将栈中元素出栈,直到栈空.
+
+    其实,我们可以发现,每次元素出栈的时候都计算了以该元素的高度为最低高度的矩阵面积,和上面的我们的dp[i]一样的思路,只是避免了一些不必要的计算.
+    最后所有元素都会依次出栈,意味着计算完了所有可能的最大面积,就可以得到结果了.
+     */
+    static public int improved(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+        for (int i = 0; i < heights.length; i++) {
+            while (!stack.empty() && heights[i] < heights[stack.peek()]) {
+                int h = heights[stack.peek()];
+                stack.pop();
+                int area = h * (stack.empty() ? i : (i - stack.peek() - 1));
+
+                max = max > area ? max : area;
+            }
+            stack.push(i);
+        }
+        while (!stack.empty()) {
+            int h = heights[stack.peek()];
+            stack.pop();
+            int area = h * (stack.empty() ? heights.length : (heights.length - stack.peek() - 1));
+
+            max = max > area ? max : area;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(largestRectangleArea(new int[]{1, 1}));
+        System.out.println(improved(new int[]{1, 1}));
+
+        System.out.println(largestRectangleArea(new int[]{2, 1, 5, 6, 2, 3}));
+        System.out.println(improved(new int[]{2, 1, 5, 6, 2, 3}));
     }
 }
