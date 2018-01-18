b8f4bcbcfcd6071bcd0ea5570cf3ed5fe65878a3
卢军球 <lujunqiu@lujunqiudeMacBook-Air.local>
2018-01-18 09:40:56 +0800

    746 dp solution

---
+++ src/Test746_Min_Cost_Climbing_Stairs.java
@@ -0,0 +1,30 @@
+/**
+ * Created by lujunqiu on 2018/1/18.
+ * Description:
+ * On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+ * Once you pay the cost, you can either climb one or two steps. You need to find minimum cost to reach the top of the floor, and you can either start from the step with index 0, or the step with index 1.
+
+ * Example 1:
+ * Input: cost = [10, 15, 20]
+ * Output: 15
+ * Explanation: Cheapest is start on cost[1], pay that cost and go to the top.
+ * Example 2:
+ * Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+ * Output: 6
+ * Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3].
+ */
+public class Test746_Min_Cost_Climbing_Stairs {
+    /**
+     * 简单的动态规划，想想斐波那契数列
+     * 状态定义dp[i]:到达第i个台阶所需的最小代价
+     * @param cost
+     * @return
+     */
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length + 1];
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[dp.length - 1];
+    }
+}
